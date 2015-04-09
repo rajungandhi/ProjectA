8d056dc35970fe9226ba4502a87e587065b684c7
rgandhi <rajungandhi@yahoo.com>
2015-04-09 15:58:28 +0530

    Added Login Class

---
+++ ProjectA/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectA.Models
+{
+    public class Login
+    {
+        public int loginID { get; set; }
+        public string  userID { get; set; }
+        public string password { get; set; }
+    }
+}
